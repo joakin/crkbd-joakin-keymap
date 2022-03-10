1a334a1425b900aeec5c993f1be6663e053738d7
joakin <joaquin@chimeces.com>
2022-03-10 15:01:56 +0100

    Fix the number layer to use normal numbers instead of keypad ones
    The keypad numbers don't work well with the terminal for some reason.

--- keymap.c
+++ keymap.c
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x5_3(
   //,--------------------------------------------.                    ,---------------------------------------------------.
-        KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                             KC_PMNS, KC_KP_7, KC_KP_8, KC_KP_9, KC_PSLS,
+        KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                             KC_MINS,    KC_7,    KC_8,    KC_9, KC_SLSH,
   //|--------+--------+--------+--------+--------|                    |---------------+--------+--------+--------+--------|
-        KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_COMM,                             KC_PPLS, KC_KP_4, KC_KP_5, KC_KP_6, KC_PAST,
+        KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_COMM,                             KC_PLUS,    KC_4,    KC_5,    KC_6, KC_ASTR,
   //|--------+--------+--------+--------+--------|                    |---------------+--------+--------+--------+--------|
-        KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_PDOT,                             KC_KP_0, KC_KP_1, KC_KP_2, KC_KP_3,KC_PEQL,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_DOT,                                KC_0,    KC_1,    KC_2,    KC_3,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------|  |--------+---------------+--------+--------+--------+--------|
                                  KC_LGUI, _______, KC_LCTL,    KC_LSFT, LT(3, KC_PENT), KC_LALT
                              //`--------------------------'  `---------------------------------'
