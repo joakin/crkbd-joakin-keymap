46d7afb292b90466439e6789f1ffb43e912ff4d6
joakin <joaquin@chimeces.com>
2022-04-05 13:48:08 +0200

    mark unused keys with XXXXXXX

--- keymap.c
+++ keymap.c
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------.                    ,--------------------------------------------.
         KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX,                      KC_MINS,    KC_7,    KC_8,    KC_9, KC_SLSH,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-        KC_F5,   KC_F6,   KC_F7,   KC_F8, KC_COMM,                      KC_PLUS,    KC_4,    KC_5,    KC_6, KC_ASTR,
+        KC_F5,   KC_F6,   KC_F7,   KC_F8, XXXXXXX,                      KC_PLUS,    KC_4,    KC_5,    KC_6, KC_ASTR,
   //|--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------|
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_DOT,                         KC_0,    KC_1,    KC_2,    KC_3,  KC_EQL,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4, XXXXXXX,                         KC_0,    KC_1,    KC_2,    KC_3,  KC_EQL,
   //|--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------|
                                  _______, KC_LGUI, KC_LCTL,    KC_LSFT, KC_LALT,   MO(3)
                              //`--------------------------'  `--------------------------'
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+---------|                    |--------+--------+--------+--------+--------|
       KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN,  KC_UNDS,                       KC_EQL, KC_LCBR, KC_RCBR, KC_COLN, KC_SCLN,
   //|--------+--------+--------+--------+---------|                    |--------+--------+--------+--------+--------|
-      KC_BSLS, KC_PIPE, KC_DQUO,  KC_GRV,  KC_TILD,                      KC_DQUO, KC_QUOT, KC_COMM,  KC_DOT, KC_SLSH,
+      KC_BSLS, KC_PIPE, XXXXXXX,  KC_GRV,  KC_TILD,                      KC_DQUO, KC_QUOT, KC_COMM,  KC_DOT, KC_SLSH,
   //|--------+--------+--------+--------+---------+--------|  |--------+--------+--------+--------+--------+--------|
                                    MO(3),  KC_LGUI, KC_LCTL,    KC_LSFT, KC_LALT, _______
                              //`---------------------------'  `--------------------------'
